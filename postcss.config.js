ab605a8b42cb0a535848f033f5196c35b01e22d6
Zipho Mashologu <zipho.m@gmail.com>
2021-09-13 01:21:55 +0200

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    require("tailwindcss"),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
@@ -11,4 +10,3 @@ module.exports = {
     })
   ]
 }
-

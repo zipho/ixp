c497411a47e2e3d4a5bf4cd95f2964a185c25926
Zipho Mashologu <zipho.m@gmail.com>
2021-09-13 12:02:56 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,64 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: {
+    enabled: ["production", "staging"].includes(process.env.NODE_ENV),
+    content: [
+      './**/*.html.erb',
+      './app/helpers/**/*.rb',
+      './app/javascript/**/*.js',
+    ],
+  },
+  presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor'},
+      flexGrow: {
+        0: '0',
+        DEFAULT: '1',
+      },
+      flexShrink: {
+        0: '0',
+        DEFAULT: '1',
+      },
+      fontFamily: {
+        sans: [
+          'Inter var',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+        mono: [
+          'ui-monospace',
+          'SFMono-Regular',
+          'Menlo'],
+        zIndex: ['responsive', 'focus-within', 'focus'],
+},
+plugins: [
+  require('@tailwindcss/typography'),
+  require('@tailwindcss/forms'),
+  require('@tailwindcss/line-clamp'),
+  require('@tailwindcss/aspect-ratio'),],
 }
